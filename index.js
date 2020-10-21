f73265d7431a89bb163487d01995e2e94b959ab7
Alexander <soudryalex@gmail.com>
2020-10-21 15:24:10 -0400

    updateing current edits.

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ app.use((err, req, res, next) => {
     res.status(500).send('Check for errors');
   });
 
-let topmovies = [
+  let movies = [
     {
-        Title: "Jurassic Park"
+        Title: "Jurassic Park",
+        Genre: "Science"
     },
     {
         Title: "Godzilla Final Wars"
@@ -47,10 +48,45 @@ app.get('/', (req, res) => {
   res.send('There is little notion more idiotic than fantasy being only being for children. The horros of reality, make it a equally enticing aspect for adults');
 });
 
-app.get('/movies', (req, res) => {                  
+app.get('/top', (req, res) => {                  
   res.json(topmovies);
 });
 
+app.get('/movies', (req, res) => {                  
+    res.send("All The Movies");
+  });
+
+app.get('/movies/[Genre]/[Title]', (req, res) => {                  
+    res.send("Single Movie");
+});
+
+app.get('/movies/[Genre]', (req, res) => {                  
+     res.send("Urban Fantasy is a genre more common for literature")
+});
+
+app.get('/movies/[Genre]/[Title]/Directors/[Name]', (req, res) => {                  
+    res.send("Spielsburg is a treasure")
+  });
+
+  app.post('/User/[Account]', (req, res) => {                  
+    res.send("Account is created")
+  });
+
+  app.put('/User/[Account]', (req, res) => {                  
+    res.send("You put your info here")
+  });
+
+  app.put('/User/[Account]/[Favorite]/[Name]', (req, res) => {                  
+    res.send("Lets add Jurrassic Park to your favorites.")
+  });
+
+  app.delete('/User/[Account]/[Favorite]/[Name]', (req, res) => {                  
+    res.send("Terminator 2 is sci fi not fantasy, get out of my favorites.");
+  });
+
+  app.delete('/User/[Account]', (req, res) => {                  
+    res.send("Remove account");
+  });
 // listen for requests
 app.listen(8080, () => {
   console.log('Listening');

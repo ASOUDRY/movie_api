a3bb22f990b49f1350acccb1f81b6cc3f920b566
Alexander <soudryalex@gmail.com>
2020-10-23 17:51:50 -0400

    Added all the HTTP requests.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express"),
 morgan = require('morgan'),
 bodyParser = require('body-parser'),
@@ -30,33 +31,34 @@ app.get('/movies/:Genre/:Title/Directors/:Name', (req, res) => {
     res.send("Spielsburg is a treasure")
   });
 
-  app.post('/Users', (req, res) => {                
+app.post('/users', (req, res) => {                
     let newuser = req.body;
   if (!newuser.name) {
     const message = 'Missing name in request body';
     res.status(400).send(message);
   } else {
     newuser.id = uuid.v4();
-    Users.push(newuser);
     res.status(201).send(newuser);
   }
 });
 
-  // app.put('/User/:Account', (req, res) => {                  
-  //   res.send("You put your info here")
-  // });
+app.put('/users/:account', (req, res) => { 
+    let update = req.body;
+      res.status(201).send(update);
+    });
 
-  // app.put('/User/:Account/:Favorite/:Name', (req, res) => {                  
-  //   res.send("Lets add Jurrassic Park to your favorites.")
-  // });
+app.put('/users/:account/:favorite/:name', (req, res) => {   
+  let update = req.body;
+      res.status(201).send(update);
+  });
 
-  // app.delete('/User/:Account/:Favorite/:Name', (req, res) => {                  
-  //   res.send("Terminator 2 is sci fi not fantasy, get out of my favorites.");
-  // });
+  app.delete('/users/:account/:favorite/:name', (req, res) => {  
+    res.status(201).send("Deleted");
+  });
 
-  // app.delete('/User/:Account', (req, res) => {                  
-  //   res.send("Remove account");
-  // });
+  app.delete('/users/:id', (req, res) => {  
+    res.status(201).send("Deletion")                
+  });
 // listen for requests
 app.listen(8080, () => {
   console.log('Listening');

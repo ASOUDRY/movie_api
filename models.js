ba5ab9220a9970f259268187367f8f984ed90f5e
Alexander <soudryalex@gmail.com>
2020-12-10 18:06:43 -0500

    Misc updates.

--- models.js
+++ models.js
@@ -20,14 +20,30 @@ let userSchema = mongoose.Schema({
     Username: {type: String, required: true},
     Password: {type: String, required: true},
     Email: {type: String, required: true},
-    Birthday: Date,
+    Birthday: {type: Date, required: true},
     FavoriteMovies: [{
         type: mongoose.Schema.Types.ObjectId, ref: 'Movie'
     }]
 });
 
+let genreSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Description: {type: String, required: true}
+});
+
+let directorSchema = mongoose.Schema({
+    Name: {type: String, required: true},
+    Bio: {type: String, required: true},
+    Birth: {type: String, required: true},
+    Death: {type: String, required: true}
+});
+
 let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User')
+let User = mongoose.model('User', userSchema);
+let Genre = mongoose.model('Genre', genreSchema);
+let Director = mongoose.model('Director', directorSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
+module.exports.Genre = Genre;
+module.exports.Director = Director;

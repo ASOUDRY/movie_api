e59bfbd1f1383caedfbf3c4d49848f01a1e2c7cf
Alexander <soudryalex@gmail.com>
2020-12-21 17:09:03 -0500

    add hash password and validate password schema.

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcrypt')
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
     Description: {type: String, required: true},
@@ -25,6 +25,14 @@ let userSchema = mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId, ref: 'Movie'
     }]
 });
+// hash password
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+  };
+// validate password
+  userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+  };
 
 let genreSchema = mongoose.Schema({
     Name: {type: String, required: true},

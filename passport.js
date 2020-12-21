ad77facf31a82f3ab73b04e3e6729454b5568ed4
Alexander <soudryalex@gmail.com>
2020-12-21 17:06:19 -0500

    add password validation.

--- passport.js
+++ passport.js
@@ -22,6 +22,11 @@ const passport = require('passport'),
         console.log('incorrect username');
         return callback(null, false, {message: 'Incorrect username or password.'});
       }
+      //  additional password code.
+      if (!user.validatePassword(password)) {
+        console.log('incorrect password');
+        return callback(null, false, {message: 'Incorrect password.'});
+      }
   
       console.log('finished');
       return callback(null, user);

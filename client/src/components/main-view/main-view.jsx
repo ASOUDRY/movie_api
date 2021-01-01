1f092bfb0cdb1d0494316bdb8cb8ff0d4e450cfc
Alexander <soudryalex@gmail.com>
2021-01-01 14:39:33 -0500

    changed heroku endpoint

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -12,7 +12,7 @@ export class MainView extends React.Component {
     }
 
     componentDidMount() {
-        axios.get('https://evening-woodland-79512.herokuapp.com/movies')
+        axios.get('https://pure-spire-30996.herokuapp.com/movies')
           .then(response => {
             // Assign the result to the state
             this.setState({

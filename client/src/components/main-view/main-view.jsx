964436757a102666962b741ced9188d3f83c503f
Alexander <soudryalex@gmail.com>
2021-01-02 17:56:23 -0500

    working on linking a button and onClick Function

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+import { Return} from '../movie-card/return';
+
 export class MainView extends React.Component {
     constructor() {
       // Call the superclass constructor
@@ -8,10 +12,15 @@ export class MainView extends React.Component {
       super();
   
       // Initialize the state to an empty object so we can destructure it later
-      this.state = {};
+      this.state = {
+        movies: null,
+        selectedMovie: null,
+        Button: null
+      };
     }
 
     componentDidMount() {
+      // axios fetches the movie end point from heroku
         axios.get('https://moviecat0l0gue.herokuapp.com/movies')
           .then(response => {
             // Assign the result to the state
@@ -23,21 +32,44 @@ export class MainView extends React.Component {
             console.log(error);
           });
       }
-
     
+      onMovieClick(movie) {
+        this.setState({
+          selectedMovie: movie
+        });
+      }
+
+      onButtonClick() {
+      axios.get('https://moviecat0l0gue.herokuapp.com/movies')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   render() {
     // If the state isn't initialized, this will throw on runtime
     // before the data is initially loaded
-    const { movies } = this.state;
+    const { movies, selectedMovie, Button } = this.state;
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view"/>;
 
     return (
-     <div className="main-view">
-     { movies.map(movie => (
-       <div className="movie-card" key={movie._id}>{movie.Title}</div>
-     ))}
+      <div className="main-view">
+      {selectedMovie
+         ? <MovieView movie={selectedMovie}/>
+         : movies.map(movie => (
+           <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
+         ? <MovieView BackButton={Button}/>
+         : <Return onClick={Button => this.onButtonClick()}/>
+         ))
+      }
      </div>
     );
   }
---
+++ client/src/components/movie-view/movie-view.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+// The movie info that is accessed via the click
+export class MovieView extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    const { movie } = this.props;
+  
+    if (!movie) return null;
+
+    return (
+      <div className="movie-view">
+        {/* keeping it commented out in case I need to add image */}
+        {/* <img className="movie-poster" src={movie.ImagePath} /> */}
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <div className="BackButton">
+          <Button>Back to List</Button>
+        </div>
+       </div>
+    );
+  }
+}

1f00461ef6d31afac0b9e5b58784f55d04c31538
Alexander <soudryalex@gmail.com>
2021-01-04 14:26:24 -0500

    removed excess lines of code

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
-import { Return} from '../movie-card/return';
 
 export class MainView extends React.Component {
     constructor() {
@@ -14,8 +13,7 @@ export class MainView extends React.Component {
       // Initialize the state to an empty object so we can destructure it later
       this.state = {
         movies: null,
-        selectedMovie: null,
-        Button: null
+        selectedMovie: null
       };
     }
 
@@ -33,44 +31,34 @@ export class MainView extends React.Component {
           });
       }
     
+  
+  
+      
       onMovieClick(movie) {
         this.setState({
           selectedMovie: movie
         });
       }
-
-      onButtonClick() {
-      axios.get('https://moviecat0l0gue.herokuapp.com/movies')
-      .then(response => {
-        // Assign the result to the state
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-
-  render() {
+  
+  
+  
+      render() {
     // If the state isn't initialized, this will throw on runtime
     // before the data is initially loaded
-    const { movies, selectedMovie, Button } = this.state;
+    const { movies, selectedMovie} = this.state;
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view"/>;
-
+  
     return (
       <div className="main-view">
-      {selectedMovie
-         ? <MovieView movie={selectedMovie}/>
-         : movies.map(movie => (
-           <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
-         ? <MovieView BackButton={Button}/>
-         : <Return onClick={Button => this.onButtonClick()}/>
-         ))
-      }
-     </div>
-    );
+       {selectedMovie
+          ? <MovieView movie={selectedMovie}/>
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
+          ))
+       }
+      </div>
+     );
   }
 }

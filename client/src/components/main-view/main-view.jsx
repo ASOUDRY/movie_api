e863afb006f74d9f8a6df138dd06b19b6c3be5e1
Alexander <soudryalex@gmail.com>
2021-01-12 17:25:49 -0500

    misc changes

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-
-// import { LoginView } from '../login-view/login-view';
+import { Register } from '../register/register';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -39,18 +39,26 @@ export class MainView extends React.Component {
         });
       }
   
-      // onLoggedIn(user) {
-      //   this.setState({
-      //     user
-      //   });
-      // } 
+      onLoggedIn(user) {
+        this.setState({
+          user
+        });
+      } 
   
+      onRegistration(user) {
+           this.setState({
+             user
+           });
+         }
+
       render() {
     // If the state isn't initialized, this will throw on runtime
     // before the data is initially loaded
     const { movies, selectedMovie, user} = this.state;
 
-    // if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+    // if (!user) return <LoginView/>;
+    // if (!user) return <Register/>;
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view"/>;

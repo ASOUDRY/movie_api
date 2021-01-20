f4d693f2291cb4c93e94cc0903b4f5a061ea524c
Alexander <soudryalex@gmail.com>
2021-01-20 16:29:12 -0500

    changing handle submit function

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -5,46 +5,28 @@ export function LoginView(props) {
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
 
-//   const handleSubmit = (e) => {
-//       e.preventDefault();
-//       axios.post('https://moviecat0l0gue.herokuapp.com/access', {
-//       Username: username,
-//       Password: password
-//       })
-//       .then(response => {
-//         console.log(username, password);
-//         const data = response.data;
-//         props.onLoggedIn(data);
-//       })
-//       .catch(e => {
-//         console.log("no such user")
-//       })
-// };
-
+const handleSubmit = (e) => {
+  e.preventDefault();
+  axios.post(`https://moviecat0l0gue.herokuapp.com/login`, {
+    Username: username,
+    Password: password
+  })
+  .then(response => {
+    const data = response.data;
+    props.onLoggedIn(data);
+  })
+  .catch(e => {
+  console.log('no user found')
+  });
+};
 
 // const handleSubmit = (e) => {
 //   e.preventDefault();
-//   axios.post(`https://moviecat0l0gue.herokuapp.com/login`, {
-//     Username: username,
-//     Password: password
-//   })
-//   .then(response => {
-//     const data = response.data;
-//     props.onLoggedIn(data);
-//   })
-//   .catch(e => {
-//   console.log('no user found')
-//   });
+// console.log(username, password);
+// /* Send a request to the server for authentication */
+// /* then call props.onLoggedIn(username) */
+// props.onLoggedIn(username);  
 // };
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-console.log(username, password);
-/* Send a request to the server for authentication */
-/* then call props.onLoggedIn(username) */
-props.onLoggedIn(username);  
-};
-
 
   return (
     <Container>

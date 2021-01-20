836117cbaae3ddff25d3ebebf76f3f5f9e2a6dde
Alexander <soudryalex@gmail.com>
2021-01-20 17:55:34 -0500

    editing app.use to test something

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 app.use(bodyParser.json());
 require('./auth')(app);
 
-app.use(cors())
+app.use(cors({ origin: '*'}))
 
 require('./passport');
 

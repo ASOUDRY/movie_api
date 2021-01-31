490954a2c84906fbb5ffb1f1040a8927e19763d4
Alexander <soudryalex@gmail.com>
2021-01-31 15:55:48 -0500

    new endpoint

--- index.js
+++ index.js
@@ -61,6 +61,16 @@ app.get('/movies/:Genre/:Title', passport.authenticate('jwt', {session: false}),
     });
 });
 
+app.get('/movies/:Genre/:Title', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Movies.find()
+    .then((movies) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
 
 app.get('/Genre', passport.authenticate('jwt', {session: false}), (req, res) => {
   Genres.find()

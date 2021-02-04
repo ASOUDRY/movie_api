f268036a0ff2f5288339e449aec3f0797871aea1
Alexander <soudryalex@gmail.com>
2021-02-04 17:27:55 -0500

    attempting to fix the get user endpoint

--- index.js
+++ index.js
@@ -105,16 +105,27 @@ app.get('/directors/:Name', passport.authenticate('jwt', {session: false}), (req
     });
 });
 
-app.get(':Username/', passport.authenticate('jwt', {session: false}), (req, res) => {
-  Users.findOne({Username: req.params.Username})
-  .then((user) => {
-    res.json(user);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send('Error: ' + err)
-  })
-})
+app.get('/users/:Name', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Users.find({Username: req.params.Name})
+    .then((movies) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// app.get(':Username/', passport.authenticate('jwt', {session: false}), (req, res) => {
+//   Users.findOne({Username: req.params.Username})
+//   .then((user) => {
+//     res.json(user);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     res.status(500).send('Error: ' + err)
+//   })
+// })
 
 app.post('/users', 
 [

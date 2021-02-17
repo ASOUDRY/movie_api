ecfd283b90402bffed5c9e47c1be28fcb6f06b0a
Alexander <soudryalex@gmail.com>
2021-02-17 12:30:48 -0500

    added the filter imput

--- client/src/components/movies-list/movies-list.jsx
+++ client/src/components/movies-list/movies-list.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
+import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input'
 import { MovieCard } from '../movie-card/movie-card';
 
 const mapStateToProps = state => {
@@ -13,17 +13,19 @@ function MoviesList(props) {
   const { movies, visibilityFilter } = props;
   let filteredMovies = movies;
 
+  console.log(filteredMovies);
+  console.log(props.setMovies);
+
   if (visibilityFilter !== '') {
     filteredMovies = movies.filter(m => m.Title.includes(visibilityFilter));
   }
 
-  if (!movies) return <div className="main-view"/>;
+  if (!movies) return <div className="SuperDiv"/>;
 
   return <div className="movies-list">
   <VisibilityFilterInput visibilityFilter={visibilityFilter} />
-  {filteredMovies.map(m => <MovieCard key={m._id} movie={m}/>)}
-</div>;
-  // filteredMovies.map(m => <MovieCard key={m._id} movie={m}/>);
+  {filteredMovies.map(m => <MovieCard key={m._id} movie={m}/>)};
+  </div>
 }
 
-export default connect(mapStateToProps)(MoviesList);
+export default connect(mapStateToProps)(MoviesList)

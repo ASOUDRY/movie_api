fe3133504c381de114c1865447e263be20fb83c1
Alexander <soudryalex@gmail.com>
2021-02-28 17:59:26 -0500

    edited the prop name.

--- client/src/components/profile/favorite.jsx
+++ client/src/components/profile/favorite.jsx
@@ -8,13 +8,14 @@ export class FavMovieCard extends React.Component {
     // This is given to the <MovieCard/> component by the outer world
     // which, in this case, is `MainView`, as `MainView` is what’s
     // connected to your database via the movies endpoint of your API
-    const { favoriteMovie } = this.props; 
+    const { favorite } = this.props; 
+    console.log(favorite);
     console.log("can you see this 2 Electric Bogoloo!");
     return (
       <Card className="fl w-50 pa2">
       <Card.Body>
-        <h5 className="card-title">{favoriteMovie.Title}</h5>
-        <Link to={`/singlemovie/${favoriteMovie.Title}/Favorite`}>
+        <h5 className="card-title">{favorite.Title}</h5>
+        <Link to={`/singlemovie/${favorite.Title}/Favorite`}>
             <Button variant="link">Open</Button>
         </Link> 
       </Card.Body>
@@ -24,6 +25,6 @@ export class FavMovieCard extends React.Component {
 }
 
 FavMovieCard.propTypes = {
-  favoriteMovie: PropTypes.shape({
+  favorite: PropTypes.shape({
   }).isRequired,
 };

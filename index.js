9edd809f8c0f1434b26209cfa8bf84dafdb8b754
Alexander <soudryalex@gmail.com>
2021-03-17 21:38:54 -0400

    removed rendundant endpoints and added a new remove endpoint.

--- index.js
+++ index.js
@@ -229,49 +229,12 @@ passport.authenticate('jwt', {session: false}), (req, res) => {
     });
   });
 
-  // test Post request
 
-  app.post('/:Username/:Image/',  
+  app.post('/:Username/:Title/:Id/:Image/Remove',  
   // passport.authenticate('jwt', {session: false}), 
   (req, res) => {
     Users.findOneAndUpdate({ Username: req.params.Username }, {
-       $push: { FavoriteMovies: {Image: req.params.Image }}
-      },
-     { new: true }, // This line makes sure that the updated document is returned
-    (err, updatedUser) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      } else {
-        res.json(updatedUser);
-      }
-    });
-  });
-
-  app.post('/:Username/:Image/Remove',  
-  // passport.authenticate('jwt', {session: false}), 
-  (req, res) => {
-    Users.findOneAndUpdate({ Username: req.params.Username }, {
-       $pull: { FavoriteMovies: {Image: req.params.Image }}
-      },
-     { new: true }, // This line makes sure that the updated document is returned
-    (err, updatedUser) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      } else {
-        res.json(updatedUser);
-      }
-    });
-  });
-  
-  app.post('/:Username/:Title/:Id/Remove', 
-  // passport.authenticate('jwt', {session: false}), 
-  (req, res) => {
-    Users.findOneAndUpdate({ Username: req.params.Username }, {
-      ImagePath: variable
-    }, {
-       $pull: { FavoriteMovies: {Title: req.params.Title, Id: req.params.Id, Image: ImagePath}}
+       $pull: { FavoriteMovies: {Title: req.params.Title, Id: req.params.Id, Image: req.params.Image }}
       },
      { new: true }, // This line makes sure that the updated document is returned
     (err, updatedUser) => {

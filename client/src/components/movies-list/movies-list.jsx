29c0f08f1873320eb49e606ea7756a0aa8605939
Alexander <soudryalex@gmail.com>
2021-03-17 22:38:20 -0400

    removeing console.log

--- client/src/components/movies-list/movies-list.jsx
+++ client/src/components/movies-list/movies-list.jsx
@@ -13,7 +13,7 @@ const mapStateToProps = state => {
 function MoviesList(props) {
   const { movies, visibilityFilter } = props;
   let filteredMovies = movies;
-  console.log(filteredMovies);
+  // console.log(filteredMovies);
 
   if (visibilityFilter !== '') {
     filteredMovies = movies.filter(m => m.Title.includes(visibilityFilter));

0e5ece2c0b15b79ed70618846159a1629a45d181
Alexander <soudryalex@gmail.com>
2021-03-21 15:55:47 -0400

    storing corrupted files for later use.

--- client/src/components/director-components/List.jsx
+++ client/src/components/director-components/List.jsx
@@ -1,35 +1,4 @@
-// import React from 'react';
-// import { connect } from 'react-redux';
-
-// import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input'
-// import {Test} from './View.jsx'
-
-// const mapStateToProps = state => {
-//   const { visibilityFilter } = state;
-//   return { visibilityFilter };
-// };
-
-// function List2(props) {
-//   const {directors, visibilityFilter} = props;
-//   console.log(directors);
-//   let filteredDirectors = directors;
-//   console.log(filteredDirectors);
-  
-//   if (visibilityFilter != '') {
-//     filteredDirectors = directors.filter(d => d.Name.includes(visibilityFilter))
-//   }
-
-//   if (!directors) return <div className="SuperDiv"/>
-
-//   return  <div>
-//     <VisibilityFilterInput visibilityFilter={visibilityFilter} />
-//     {filteredDirectors.map(d => <Test key={d._id} director={d}/>)}
-//   </div>
-// }
-
-// export default connect(mapStateToProps)(List2)
-
-
+// import { response } from 'express';
 import React from 'react';
 import { connect } from 'react-redux';
 
@@ -44,17 +13,22 @@ const mapStateToProps = state => {
 function DirectorList(props) {
   const { directors, visibilityFilter } = props;
   let filteredDirectors = directors;
-  console.log(filteredDirectors)
+  // console.log(filteredDirectors)
 
   if (visibilityFilter !== '') {
     filteredDirectors = directors.filter(d => d.Name.includes(visibilityFilter));
+  }
+
+  // this is the problem
+  function test(test) {
+    console.log(test)
   }
 
   if (!directors) return <div className="main-view"/>;
 
   return <div>
        <VisibilityFilterInput visibilityFilter={visibilityFilter} />
-  {filteredDirectors.map(d => <DirectorView key={d._id} director={d}/>)};
+  {filteredDirectors.map(d => <DirectorView key={d._id} director={d} test={test => this.test(test)}/>)};
   </div>
 }
 
--- client/src/components/director-components/director-view.jsx
+++ client/src/components/director-components/director-view.jsx
@@ -3,47 +3,64 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card, Container, CardColumns } from 'react-bootstrap';
 import { Link } from "react-router-dom";
+import axios from 'axios';
 // import './genre-view.scss';
 
 export class DirectorView extends React.Component {
+
+  directorProp(directorTag) {
+    // console.log(directorTag)
+    let token = localStorage.getItem('token')
+    // let directors = []
+    // console.log(token);
+    axios.get(`https://moviecat0l0gue.herokuapp.com/directors/${directorTag}`, {
+      headers: { Authorization: `Bearer ${token}`}
+    })
+    .then(response => {
+      // console.log(response.data);
+      let test = "test";
+      console.log(test);
+      // this.props.Upward(test);
+    })
+  }
+
+  test() {
+    test = this.test
+    this.props.test(test);
+  }
+  
   render() {
     // This is given to the <MovieCard/> component by the outer world
     // which, in this case, is `MainView`, as `MainView` is what’s
     // connected to your database via the movies endpoint of your API
     const { director } = this.props;
+    const directorTag = director.DTag
     const directorName = director.Name;
-    // const fetching = (e) => {
-    //   console.log(genreName);
-    //   e.preventDefault();
-    //   this.props.genreProp(genreName)
-    // }
 
     return (
       <div>
-        {/* <Card>
-        <img src="https://i.pinimg.com/originals/c5/54/b9/c554b9b17299fa8adf539a5fba7ea3fd.jpg" alt="Snow" style="width:100%;"></img>
-        <div class="centered">Centered</div>
-        </Card> */}
-
       <Card className="fl w-50 pa2" >
       <Card.Body>
         <h5 className="card-title">{directorName}</h5>
         <p className="card-text">{director.Description}</p>
-        <Link to={`/Genre/${directorName}`}>
-        <Button onClick={
+        {/* <Link to={`/Director/${directorName}`}> */}
+        {/* <Button onClick={
           () => {
-            this.props.directorProp(directorName)
+            // props.testingFile()
+            // this.directorProp(director.Itag)
+            this.test()
+            // console.log(this.state.directorMovie);
           }}
-          >View Movies</Button>
-        </Link>
+          >View Movies</Button> */}
+        {/* </Link> */}
+        <Button>View Movies</Button>
       </Card.Body>
       </Card>
       </div>
-
     )
   }
 }
-
+ 
 DirectorView.propTypes = {
   director: PropTypes.shape({
     Name: PropTypes.string,
